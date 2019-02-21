a02680e5c5955b41b84bc412cab61481996fba67
Michele <ognioscar61@gmail.com>
2019-02-21 09:50:15 +0100

    Creazione del programma ed inizializzazione delle variabili

---
+++ OgnibenMichele_A_TestReazione/OgnibenMichele_A_TestReazione.ino
@@ -0,0 +1,30 @@
+            //  input
+int button1;
+int button2;
+int button3;
+            //  output
+int ledTest;
+int ledEsito;
+int buzzer;
+
+void setup() {
+              //  assegnazione variabili
+  button1  = ;
+  button2  = ;
+  button3  = ;
+  ledTest  = ;
+  ledEsito = ;
+  buzzer   = ;
+                          //  input
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+                          //  output
+  pinMode(ledTest, OUTPUT);
+  pinMode(ledEsito, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+
+}
